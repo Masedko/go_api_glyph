547c7bd40faa233014ddf0810d98a3af3a271b8b
Masedko <andruugladkuu@gmail.com>
2022-08-26 10:37:43 +0300

    favicon.ico handle

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 	s.Docs("/docs", v4emb.New)
 
 	log.Println("Starting service")
-	if err := http.ListenAndServe("localhost:8080", s); err != nil {
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), s); err != nil {
 		log.Fatal(err)
 	}
 }
